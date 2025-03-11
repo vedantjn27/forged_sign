48c2819cf5ea27871b7f41e86af5144919c978fc
vedantjn27 <vedantjain273@gmail.com>
2025-03-12 00:58:07 +0530

    changed to deploy file

--- testing.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-
-# Read the CSV with detected encoding
-df = pd.read_csv("train_data_utf8.csv", encoding="cp1252")
-
-# Save it again with utf-8 encoding
-df.to_csv("train_data_utf8.csv", encoding="utf-8", index=False)
-
